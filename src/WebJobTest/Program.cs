476dbfed4d15f6f5e086821141618efde94c3036
Artur Karbone <artur.karbone@gmail.com>
2016-10-31 17:50:02 +0200

    Initial

---
+++ src/WebJobTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace WebJobTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.WriteLine($"Current time is {DateTime.Now}");
+                Thread.Sleep(3000);
+            }
+        }
+    }
+}
